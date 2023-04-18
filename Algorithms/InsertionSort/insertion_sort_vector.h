e14a7370a8a8117430d23ce34ddead9c6063f5be
Elen8 <90869008+Elen8@users.noreply.github.com>
2023-04-18 15:51:01 +0400

    Create insertion_sort_vector.h

---
+++ Algorithms/InsertionSort/insertion_sort_vector.h
@@ -0,0 +1,10 @@
+#ifndef INSERTION_SORT_VECTOR
+#define INSERTION_SORT_VECTOR
+#include <vector>
+#include <iostream>
+
+void insertion_sort_vector(std::vector<int>&, size_t&);
+void input_data_vector(std::vector<int>&, size_t&);
+void output_vector(std::vector<int>&, size_t&);
+
+#endif
