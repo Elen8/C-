f1e7b750a7ae610dd138818b9168a577ddddc4a5
Elen8 <90869008+Elen8@users.noreply.github.com>
2023-04-18 15:50:19 +0400

    Create insertion_sort_unique.h

---
+++ Algorithms/InsertionSort/insertion_sort_unique.h
@@ -0,0 +1,10 @@
+#ifndef INSERTION_SORT_UNIQUE
+#define INSERTION_SORT_UNIQUE
+#include <iostream>
+#include <memory>
+
+void insertion_sort_unique(std::unique_ptr<int[]>&, size_t&);
+void input_data_unique(std::unique_ptr<int[]>&, size_t&);
+void output_unique(std::unique_ptr<int[]>&, size_t&);
+
+#endif
