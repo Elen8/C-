4a3cb0f15d84452ef7ae50ec30ea64cf10220c4f
Elen8 <90869008+Elen8@users.noreply.github.com>
2023-04-23 11:12:13 +0400

    Create merge_sort.h

---
+++ Algorithms/MergeSort/merge_sort.h
@@ -0,0 +1,10 @@
+#ifndef MERGE_SORT
+#define MERGE_SORT
+
+void merge(int*&, int, int, int);
+void merge_sort(int*&, int, int);
+
+void set_data(int*&, int);
+void get_data(int*&, int);
+
+#endif //MERGE_SORT
